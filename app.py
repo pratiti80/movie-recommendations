77118710b408738f8e6e803dd28a469b9f61c344
Pratiti Pai <pratitipie55@gmail.com>
2024-10-22 21:09:14 -0700

    1st version of app.py

--- app.py
+++ app.py
@@ -1,5 +1,31 @@
-import os 
-from dotenv import load_dotenv
-load dotenv()
+from flask import Flask, render_template, request
+import requests
 
-API_KEY = os.getenv('TMBD_API_KEY')
+app = Flask(__name__)
+
+API_KEY = 'c2aedca4f9a18e61a95892ff8a18f401'
+BASE_URL = 'https://api.themoviedb.org/3'
+
+
+@app.route('/')
+def index():
+  return render_template('index_html')
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        query = request.form.get('movie_query')
+        search_url = f"{BASE_URL}/search/movie?api_key={API_KEY}&query={query}"
+        response = requests.get(search_url)
+        data = response.json()
+        return render_template('search_results.html', movies=data['results'], query=query)
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+  
+
+
+
+
